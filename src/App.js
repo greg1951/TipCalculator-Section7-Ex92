329fbfa02dcd89c2bf60cdf44a8058132845c5ab
Greg Hughlett <greg.hughlett@gmail.com>
2025-05-01 00:09:26 +0000

    checking in a very challenging code challenge

--- src/App.js
+++ src/App.js
@@ -1,36 +1,84 @@
 import "./styles.css";
+import { useState } from "react";
 
 export default function App() {
-  const serviceRatings = [
-    "BAD (0%)",
-    "OK (10%)",
-    "GOOD (15%)",
-    "AMAZING (20%)",
-  ];
-
   return (
-    <div className="App">
-      <BillAmount />
-      <h3>Pay $0 ($0 (Check) + $0 (Tip)</h3>
-      <button type="reset">Reset</button>
+    <div>
+      <TipCalculator />
     </div>
   );
 
-  function BillAmount() {
+  function TipCalculator() {
+    const [billAmount, setBillAmount] = useState("");
+    const [party1Pct, setParty1Pct] = useState(0);
+    const [party2Pct, setParty2Pct] = useState(0);
+
+    const tip = billAmount * ((party1Pct + party2Pct) / 2 / 100);
+
+    return (
+      <div className="App">
+        <BillAmount billAmount={billAmount} onBillChange={setBillAmount} />
+        <SelectTipPercentage percent={party1Pct} onSelect={setParty1Pct}>
+          {"How did you like the service?"}
+        </SelectTipPercentage>
+        <SelectTipPercentage percent={party2Pct} onSelect={setParty2Pct}>
+          {"How did your friend like the service?"}
+        </SelectTipPercentage>
+        {billAmount > 0 && (
+          <>
+            <FinalPayment billAmount={billAmount} tip={tip} />
+            <Reset onReset={handleReset} />
+          </>
+        )}
+      </div>
+    );
+    function handleReset() {
+      setParty1Pct(0);
+      setParty2Pct(0);
+      setBillAmount("");
+    }
+  }
+
+  function BillAmount({ billAmount, onBillChange }) {
     return (
       <div>
-        <form>
-          <label for="checkAmount">What is the check amount?</label>
-          <input
-            type="number"
-            name="checkAmount"
-            value="0"
-            id="checkAmount"
-          ></input>
-        </form>
+        <label>What is the check amount?</label>
+        <input
+          type="text"
+          placeholder="Bill Amount"
+          value={billAmount}
+          onChange={(e) => onBillChange(Number(e.target.value))}
+        />
       </div>
     );
   }
-  function Party1Tip() {}
-  function Party2Tip() {}
+  function SelectTipPercentage({ children, percent, onSelect }) {
+    return (
+      <div>
+        <label>{children}</label>
+        <select
+          value={percent}
+          onChange={(e) => onSelect(Number(e.target.value))}
+        >
+          <option value="0">HORRIBLE</option>
+          <option value="5">NOT GOOD</option>
+          <option value="10">OK</option>
+          <option value="15">GOOD</option>
+          <option value="20">RAD</option>
+        </select>
+      </div>
+    );
+  }
+  function FinalPayment({ billAmount, tip }) {
+    return (
+      <div>
+        <h3>
+          Pay total: ${billAmount + tip} (Check: ${billAmount} + Tip: ${tip})
+        </h3>
+      </div>
+    );
+  }
+  function Reset({ onReset }) {
+    return <button onClick={onReset}>Reset</button>;
+  }
 }
